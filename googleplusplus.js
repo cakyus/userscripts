63b6a81c05cdd110d39096fceaf2f6fc55a8b820
cakyus <cakyus@gmail.com>
2012-09-05 06:07:30 +0700

    add copyright information

--- googleplusplus.js
+++ googleplusplus.js
@@ -1,3 +1,22 @@
+// googleplusplus.js
+// Copyright (c) 2010-2012, Yus Uf
+// Released under the GPL license
+// http://www.gnu.org/copyleft/gpl.html
+//
+// --------------------------------------------------------------------
+//
+// This is a Greasemonkey user script.
+//
+// To install, you need Greasemonkey: http://www.greasespot.net/
+// Then restart Firefox and revisit this script.
+// Under Tools, there will be a new menu item to "Install User Script".
+// Accept the default configuration and install.
+//
+// To uninstall, go to Tools/Manage User Scripts,
+// select "googleplusplus.js", and click Uninstall.
+//
+// --------------------------------------------------------------------
+//
 // ==UserScript==
 // @id             c2892b32-c3e1-4cf1-a4f6-e9d278cfd897
 // @name           Google++

7be4699752c0e44d0dd9150e391bc15a11a08e87
cakyus <cakyus@gmail.com>
2012-10-31 11:17:36 +0700

    move global code into NoteJS.init()

--- note.user.js
+++ note.user.js
@@ -9,54 +9,43 @@
 // @run-at         document-end
 // ==/UserScript==
 
-// == GreaseMonkey Compatibility
+// ==GreaseMonkey==
 
 if (typeof GM_addStyle != 'function'){
 	function GM_addStyle(textContent) {
 		var oHead = document.getElementsByTagName('head')[0];
 		var oStyle = document.createElement('style');
 		oStyle.type = 'text/css';
-		oStyle.textContent =
-			  'body {margin: 5px auto; text-align: center; }'
-			+ 'body {background-image: -moz-radial-gradient(left top, ellipse farthest-side, #FCFFFD 0%, #B1EFBB 80%);}'
-			+ '#note-content { width: 65%; height: 90%;}'
-			+ '#note-content { padding: 10px; border: 1px solid #ddd; }'
-			+ '#note-content { font-family: Monospace, Consolas; font-size: x-large;}'
-			+ '#command-show { font-size: large; margin-bottom: 10px;}'
-			;
+		oStyle.textContent = textContent;
 		oHead.appendChild(oStyle);
 	}	
 }
 
-var oBody = document.getElementsByTagName('body')[0];
-
-// add style
-GM_addStyle();
-
-var oCommandButtons = document.createElement('div');
-oCommandButtons.id = 'command-buttons';
-oBody.appendChild(oCommandButtons);
-
-var oCommandSwitchContent = document.createElement('input');
-oCommandSwitchContent.id = 'command-show';
-oCommandSwitchContent.type = 'button';
-oCommandSwitchContent.value = 'Show Original';
-oCommandButtons.appendChild(oCommandSwitchContent);
-
-var oContent = document.createElement('textarea');
-oContent.id = 'note-content';
-oBody.appendChild(oContent);
+// ==/GreaseMonkey==
 
 var oNoteJS = new NoteJS;
-oContent.textContent = oNoteJS.getContentOriginal();
-oContent.addEventListener('keyup', function() {
-	oNoteJS.setContent(oContent.value);
-});
+oNoteJS.init();
 
 function NoteJS() {
 
 	this.contentOriginal = unescape(document.location.href).substr(12);
 	this.content = '';
+	
+	this.init = function() {
+		GM_addStyle(
+			  'body {margin: 5px auto; text-align: center; background-image: -moz-radial-gradient(left top, ellipse farthest-side, #FCFFFD 0%, #B1EFBB 80%);}'
+			+ '#note-content { width: 65%; height: 90%;}'
+			+ '#note-content { padding: 10px; border: 1px solid #ddd; }'
+			+ '#note-content { font-family: Monospace, Consolas; font-size: x-large;}'
+			+ '#command-show { font-size: large; margin-bottom: 10px;}'
+		);
+		document.body.innerHTML = '<div id="command-buttons"><input value="Show Original" id="command-show" type="button"></div><textarea id="note-content"></textarea>"';
+		var oContent = document.getElementById('note-content');
+		oContent.textContent = oNoteJS.getContentOriginal();
+		oContent.addEventListener('keyup', function() {
+			oNoteJS.setContent(oContent.value);
+		});
+	}
 
 	this.getContentCurrent = function() {
 		return unescape(document.location.href).substr(12);

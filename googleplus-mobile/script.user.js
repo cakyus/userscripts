038d1e2fa130fbdb9617f7c68ef24b39c1d93ddf
cakyus <cakyus@gmail.com>
2013-03-14 22:58:46 +0700

    [bugfix] pattern of post-item's location

--- googleplus-mobile/script.user.js
+++ googleplus-mobile/script.user.js
@@ -32,6 +32,7 @@
 // 2013-03-13 [feature] circle page >> post item >> user link : open link in new window
 // 2013-03-13 [feature] circle page >> post item >> links : open link in new window
 // 2013-03-13 [bugfix] latest comments won't hide in circles page
+// 2013-03-13 [bugfix] pattern of post-item's location
 
 // TODO
 // [bugfix] there are two posts which are not separated
@@ -49,10 +50,10 @@ $(document).ready(function(){
     // .jqF9Bc  comment button at the bottom of post item
     // #23 img  images in post item content
 
-    GM_addStyle('body { background-color: #D4DDF0 ; width: 58%; margin-left: 10px; } .xFZOtc { background-color: #FFF; margin-top: 10px; font-size: 20px; font-family: Ubuntu Condensed; } .VSlytb { border-top: 1px solid #DDD; padding-bottom: 10px;} .jfc, .MQtqDc, .VSlytb { display: none; } div#23 img { width: 20%; padding: 10px; } .sidebar { position: fixed; top: 0px; right: 2px; min-height: 100px; background-color: #fff; min-width: 200px; padding:10px; } .sidebar a { color: #000; display: block; padding: 5px; } .sidebar a:hover { background-color: #DDD; } .sidebar h2 { padding: 0px; } .menu-item { display: block; padding: 5px 10px; }');
+    GM_addStyle('body { background-color: #D4DDF0 ; width: 58%; margin-left: 10px; } .qry4Ge, .Ie0wAe, .Lfc, .CMuu5c, .OuNFwc { padding: 5px; margin: 0px; } .OuNFwc, .qry4Ge { background-color: #FFF; font-size: 20px; font-family: Ubuntu Condensed; } .OuNFwc, .Ie0wAe { width: 100%; } .CMuu5c { display:block; width: 100%; background-color: #FFF; } .VSlytb { border-top: 1px solid #DDD; padding: 5px 5px 10px 5px; width: 100%; } .jfc, .MQtqDc, .VSlytb, .gfc { display: none; } div#23 img { width: 20%; padding: 10px; } .VSlytb .Lfc { display: inline; } .sidebar { position: fixed; top: 0px; right: 2px; min-height: 100px; background-color: #fff; min-width: 200px; padding:10px; } .sidebar a { color: #000; display: block; padding: 5px; } .sidebar a:hover { background-color: #DDD; } .sidebar h2 { padding: 0px; } .menu-item { display: block; padding: 5px 10px; }');
 
     // style for post detail
-    if (location.href.match('/stream/[a-z0-9]{35,35}') != null){
+    if (location.href.match('/stream/[a-z0-9]{32,}') != null){
         GM_addStyle('.VSlytb { display: block; }');
     }
 
